c0de6162c1ce86371ae27468089822ee584963c0
romulodf.cesar <romulodf.cesar@gmail.com>
2026-03-24 17:49:32 -0300

    oi

--- associado/admin.py
+++ associado/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from associado.models import Associado
+from associado.models import Empresa
 # Register your models here.
+admin.site.register(Associado)
+admin.site.register(Empresa)

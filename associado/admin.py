b815887eb83bf5ddc4ba1fcaef270e705afd556c
romulodf.cesar <romulodf.cesar@gmail.com>
2026-03-30 17:46:49 -0300

    ok

--- associado/admin.py
+++ associado/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from associado.models import Associado
 from associado.models import Empresa
+
+class ListandoAssociados(admin.ModelAdmin):
+    list_display = ('nome', 'status', 'fk_empresa_id')
+    search_fields = ('nome', 'status')
+
 # Register your models here.
-admin.site.register(Associado)
+admin.site.register(Associado, ListandoAssociados)
 admin.site.register(Empresa)
--- associado/models.py
+++ associado/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Associado(models.Model):
+    LISTA_STATUS = [
+        ('ATIVO', 'Ativo'),
+        ('INATIVO', 'Inativo'),
+        ('PENDENTE', 'Pendente'),
+    ]
     nome = models.CharField(max_length=80,null=False,blank=False)
-    entidade = models.CharField(max_length=80,null=False,blank=False)
-    status = models.CharField(max_length=15,null=False,blank=False)
-
+    status = models.CharField(max_length=15,null=False,blank=False,choices=LISTA_STATUS)
+    # criar um atributo fk_empresa_id que seja uma chave estrangeira para a tabela empresa
+    fk_empresa_id = models.ForeignKey('Empresa', on_delete=models.CASCADE, null=True, blank=True,verbose_name='Empresa')
     def __str__(self):
-        return f"dados do objeto: {self.nome} - {self.entidade} - {self.status}"
+        return f"dados do objeto: {self.nome} - {self.fk_empresa_id} - {self.status}"
+# faça uma chave estrangeira com empresa
+
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=80,null=False,blank=False)
     cnpj = models.CharField(max_length=16,null=False,blank=False)
+    cidade = models.CharField(max_length=30,null=True,blank=True)
     def __str__(self):
-        return f"dados do objeto: {self.nome} - {self.cnpj}"
+        return f"{self.nome}"    
